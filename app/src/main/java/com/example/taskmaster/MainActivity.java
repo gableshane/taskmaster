70a767d1224e49171e56932649cea3e088048a39
Shane Gable <gableshane@gmail.com>
2020-02-11 11:21:17 -0800

    completed day 1

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button addTaskButton = findViewById(R.id.addTask);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAddTaskIntent = new Intent(MainActivity.this, AddTask.class);
+                MainActivity.this.startActivity(goToAddTaskIntent);
+            }
+        });
+
+        Button allTaskButton = findViewById(R.id.allTasks);
+        allTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToAllTasks = new Intent(MainActivity.this, AllTasks.class);
+                MainActivity.this.startActivity(goToAllTasks);
+            }
+        });
+
     }
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+This is my Task Master app day 1. So far, I have created a home page with two buttons that take you to an Add Task page or a All Tasks page. This is a screenshot of the home page:
+
+![image description](screenshots/home.png)
+
+The Add Task page allows you to add a task with a title and a description. When the Add Task button is clicked, you create an instance of a task with that title and description. I have written unit tests for the task class.
+
+The All Tasks page is just a simple Android activity with a small image per the instructions.
